4f2763de7a29b834ba4107365af9ced1eed62d01
amsigo <koeunhh5@naver.com>
2026-02-19 03:27:46 +0900

    [fix] 404 에러 관련 수정

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
 })
